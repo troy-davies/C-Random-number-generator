57e914e68ecdb00efb6651994932610f5686bfa1
Troy-Davies <164232406+troy-davies@users.noreply.github.com>
2025-12-07 10:19:06 +0000

    Update random number generation using libsodium
    Replaced the simple random number generation with libsodium for better randomness and security.

--- main.c
+++ main.c
@@ -1,20 +1,22 @@
 // Simple random number generator made by Troy Davies.
+#include <sodium.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 int main() {
-    srand(time(0));
-    //Seed the gen
-    int min, max;
+    if (sodium_init() < 1) { // Initiallise sodium
+        return 1;
+    }
+    int min, max, random;
+    randombytes(&random, sizeof(int));
     printf("Input minimum value.\nValue: ");
     scanf("%d", &min);
     printf("Input maximum value.\nValue: ");
     scanf("%d", &max);
     if(max < min) {
     printf("ERROR: Maximum value must be larger than minimum value.\n");
-    return 1; // Make sure that there is a proper range of numbers.
+    return 1; // Make sure that there is a proper range of numbers
     }
-    int randnum = (rand() % (max - min + 1)) + min;
+    int randnum = min + randombytes_uniform(max - min + 1) // Generate random number
     printf("Random number is %d.\n", randnum);
     return 0;
+
 }
